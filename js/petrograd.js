fee0a7c08895f6570866764823fe0762babc4d2c
Frederikke Holmen <frederikkeholmen@desktop-pd9m040.kea.local>
2017-02-23 13:27:34 +0100

    remove udsolgt tekst when not udsolgt

--- js/petrograd.js
+++ js/petrograd.js
@@ -31,6 +31,15 @@ function visProdukt(produkt) {
 
     klon.querySelector(".data_billede").src = "/imgs/small/" + produkt.billede + "-sm.jpg";
 
+    if (produkt.udsolgt == false) {
+        //Produktet er ikke udsolgt
+        //udsolgtteskt skal fjernes
+        var udsolgttekst = klon.querySelector(".udsolgttekst");
+        udsolgttekst.parentNode.removeChild(udsolgttekst);
+    } else {
+        klon.querySelector(".pris").classList.add("udsolgt");
+    }
+
     //append klon til .produkt_liste
     document.querySelector(".produktliste").appendChild(klon);
 }
